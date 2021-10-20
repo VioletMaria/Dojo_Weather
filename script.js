e0e976e52f308b6250c3edb8dfccdf2e4941c149
Violetta Szewczyk <szewczykviolet@gmail.com>
2021-10-19 21:49:50 -0500

    iffy commit

--- script.js
+++ script.js
@@ -8,3 +8,47 @@ function showAlert(){
         alert.style.visibility = 'hidden';
     }
 }
+
+function cToF(temp){
+    return Math.round(9 / 5 * temp + 32);
+}
+
+function fToC(temp){
+    return Math.round(5 / 9 * (temp - 32));
+} 
+
+function farOrCel(element) {
+        console.log(element.value);
+        for(var i=1; i<9; i++) {
+            var tempSpan = document.querySelector(".temp" + i);
+            var tempVal = parseInt(tempSpan.innerText);
+            if(element.value == "°C") {
+                tempSpan.innerText = fToC(tempVal) + "°";
+            } else {
+                tempSpan.innerText = cToF(tempVal) + "°";
+            }
+        }
+    }
+
+// solution:
+
+// function c2f(temp) {
+//     return Math.round(9 / 5 * temp + 32);
+// }
+
+// function f2c(temp) {
+//     return Math.round(5 / 9 * (temp - 32));
+// }
+
+// function convert(element) {
+//     console.log(element.value);
+//     for(var i=1; i<9; i++) {
+//         var tempSpan = document.querySelector("#temp" + i);
+//         var tempVal = parseInt(tempSpan.innerText);
+//         if(element.value == "°C") {
+//             tempSpan.innerText = f2c(tempVal);
+//         } else {
+//             tempSpan.innerText = c2f(tempVal);
+//         }
+//     }
+// }
